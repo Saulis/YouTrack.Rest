a0379dd1a965c9c1f8496c8e9e3bfb19bf6bdb88
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-07-06 19:28:36 +0300

    Refactored IssueFactory.

--- YouTrack.Rest/Factories/IssueProxyFactory.cs
+++ YouTrack.Rest/Factories/IssueProxyFactory.cs
@@ -9,12 +9,4 @@
             return CreateProxy<IIssue>(issue);
         }
     }
-
-    class IssueFactory : IIssueFactory
-    {
-        public IIssue CreateIssue(string issueId, IConnection connection, IIssueRequestFactory issueRequestFactory)
-        {
-            return new Issue(issueId, connection, issueRequestFactory);
-        }
-    }
 }
