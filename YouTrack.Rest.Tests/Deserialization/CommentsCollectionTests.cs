265563d3c6a296489f86a7d927115c530e8c109d
Sauli <sauli.tahkapaa@netti.fi>
2012-07-11 17:02:16 +0300

    Improved CommentsCollection testing

---
+++ YouTrack.Rest.Tests/Deserialization/CommentsCollectionTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using YouTrack.Rest.Deserialization;
+
+namespace YouTrack.Rest.Tests.Deserialization
+{
+    class CommentsCollectionTests : TestFor<CommentsCollection>
+    {
+        private Rest.Deserialization.Comment deserializedComment;
+        private List<Rest.Deserialization.Comment> deserializedComments;
+        private IConnection connection;
+
+        protected override void SetupDependencies()
+        {
+            connection = Mock<IConnection>();
+
+            deserializedComments = new List<Rest.Deserialization.Comment>();
+            deserializedComment = Mock<Rest.Deserialization.Comment>();
+            deserializedComments.Add(deserializedComment);
+
+            Sut.Comments = deserializedComments;
+        }
+
+        [Test]
+        public void GetCommentCalledOnEachComment()
+        {
+            IEnumerable<IComment> comments = Sut.GetComments(connection).ToList();
+
+            deserializedComment.Received().GetComment(connection);
+        }
+    }
+}
