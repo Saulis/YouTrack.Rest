1bd5e53326c51a20968551d1b398a406eaaca6ad
Sauli <sauli.tahkapaa@netti.fi>
2013-06-09 21:29:45 +0300

    Again fiddling with nuspec.

--- YouTrack.Rest/Properties/AssemblyInfo.cs
+++ YouTrack.Rest/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("YouTrack.Rest")]
-[assembly: AssemblyCopyright("Copyright © Sauli Tähkäpää 2012")]
+[assembly: AssemblyCopyright("Copyright © Sauli Tähkäpää 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
